aeffce26036cb7b502ae4c027030fff7da80fd03
LuanAlvesRoc <112912482+LuanAlvesRoc@users.noreply.github.com>
2022-12-06 15:43:43 -0300

    Create Calculo6.java

---
+++ atividade06/Calculo6.java
@@ -0,0 +1,32 @@
+package atividade06;
+import java.util.Scanner;
+public class Calculo6 {
+	Scanner entrada = new Scanner(System.in);
+	 double horasTrabalhadas;
+    double Desconto;
+    double TotalDesconto;
+    double salarioLiquido;
+    double salarioBruto;
+    double valorHora;
+    int formula = 100;
+    
+	
+	    public void cauculoDeSalario() {
+
+	        System.out.println("insira as horas trabalhadas");
+	        horasTrabalhadas = entrada.nextDouble();
+
+	        System.out.println("insira o valor da hora");
+	        valorHora = entrada.nextDouble();
+
+	        System.out.println("insira o percentual de desconto");
+	        Desconto = entrada.nextDouble();
+
+	        salarioBruto = horasTrabalhadas * valorHora;
+	        TotalDesconto = Desconto / 100 / salarioBruto;
+	        salarioLiquido = salarioBruto - TotalDesconto;
+
+	        System.out.println("o salario bruto é = "+ salarioBruto + "\no totalDesconto é = "+TotalDesconto+ "\no salario liquido é = "+salarioLiquido);
+	    }
+
+}
