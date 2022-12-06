a3ac42eae9504c63310433bc6f9b4caf5654cb0a
LuanAlvesRoc <112912482+LuanAlvesRoc@users.noreply.github.com>
2022-12-06 15:53:04 -0300

    Create Calculo14.java

---
+++ atividade14/Calculo14.java
@@ -0,0 +1,64 @@
+package atividade14;
+import java.util.ArrayList;
+public class Calculo14 {
+	 String modelo; 
+	 String memoria;
+	 String armazenamento;
+	 String processador;
+	 String placaDeVideo;
+	 ArrayList<Calculo14> listaQ14 = new ArrayList<>();
+	
+	public Calculo14(
+			String modelo,
+			String memoria, 
+			String armazenamento, 
+			String processador, 
+			String placaDeVideo) {
+		this.modelo = modelo;
+		this.memoria = memoria;
+		this.armazenamento = armazenamento;
+		this.processador = processador;
+		this.placaDeVideo = placaDeVideo;
+		
+	}
+	
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getMemoria() {
+		return memoria;
+	}
+
+	public void setMemoria(String memoria) {
+		this.memoria = memoria;
+	}
+
+	public String getArmazenamento() {
+		return armazenamento;
+	}
+
+	public void setArmazenamento(String armazenamento) {
+		this.armazenamento = armazenamento;
+	}
+
+	public String getProcessador() {
+		return processador;
+	}
+
+	public void setProcessador(String processador) {
+		this.processador = processador;
+	}
+
+	public String getPlacaDeVideo() {
+		return placaDeVideo;
+	}
+
+	public void setPlacaDeVideo(String placaDeVideo) {
+		this.placaDeVideo = placaDeVideo;
+	}
+}
