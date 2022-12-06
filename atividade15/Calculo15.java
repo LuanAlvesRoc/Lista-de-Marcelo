23e9bd49a91ad0e52fd15de4ff2c6f18f0c4493e
LuanAlvesRoc <112912482+LuanAlvesRoc@users.noreply.github.com>
2022-12-06 15:56:59 -0300

    Create Calculo15.java

---
+++ atividade15/Calculo15.java
@@ -0,0 +1,18 @@
+package atividade15;
+import java.util.Scanner;
+public class Calculo15 {
+
+	Scanner scan = new Scanner(System.in);
+	String livro, nome;
+	
+	    public void livro() {
+	        
+	        System.out.println("Digite o seu nome");
+	        nome = scan.nextLine();
+	        
+	        System.out.println("Digite o nome do livro");
+	        livro = scan.nextLine();
+
+	        System.out.println("  Aqui está = o livro " +livro+ "  será emprestado para: " +nome);
+	        }
+}
