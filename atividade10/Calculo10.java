30888b29bf4a7397a7011e2fbbcd798a1d8dde16
LuanAlvesRoc <112912482+LuanAlvesRoc@users.noreply.github.com>
2022-12-06 15:49:01 -0300

    Create Calculo10.java

---
+++ atividade10/Calculo10.java
@@ -0,0 +1,10 @@
+package atividade10;
+
+public class Calculo10 {
+	public static void main(String[] args) {
+	      for (int x = 100; x>= 1; x--){
+	        System.out.print(x+"");
+	      }
+	       
+	    }
+}
