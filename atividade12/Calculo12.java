ce9ee9b547fd16437892b75cf8f8c5e3d548f750
LuanAlvesRoc <112912482+LuanAlvesRoc@users.noreply.github.com>
2022-12-06 15:51:04 -0300

    Create Calculo12.java

---
+++ atividade12/Calculo12.java
@@ -0,0 +1,13 @@
+package atividade12;
+
+public class Calculo12 {
+	public void tela() {	
+		System.out.print("| Opção 1 - Aprender \n");
+        System.out.print("| Opção 2 - Escrever \n");
+        System.out.print("| Opção 3 - Falar \n");
+        System.out.print("| Opção 0 - Sair     \n");
+        System.out.print("Digite uma opção: ");
+        
+   
+	}         
+}
