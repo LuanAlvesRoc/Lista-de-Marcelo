5fb9c9045cf182a48b2d3e30a7c3f59ca7cc871e
LuanAlvesRoc <112912482+LuanAlvesRoc@users.noreply.github.com>
2022-12-06 15:45:22 -0300

    Create Calculo7.java

---
+++ atividade07/Calculo7.java
@@ -0,0 +1,12 @@
+package atividade07;
+import java.util.Scanner;
+public class Calculo7 {
+	public static void main(String[] args) {
+	 	Bhaskara objeto = new Bhaskara();
+        double valordelta = objeto.delta();
+        System.out.println("Delta = " + valordelta);
+        if (valordelta<0) System.out.print("Delta menor que zero");
+        else objeto.raizes(valordelta);
+        System.out.println();  
+    }
+}
