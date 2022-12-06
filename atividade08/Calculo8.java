33fbbf108fbd366b8d56a531aac29c64afc49d42
LuanAlvesRoc <112912482+LuanAlvesRoc@users.noreply.github.com>
2022-12-06 15:46:09 -0300

    Create Calculo8.java

---
+++ atividade08/Calculo8.java
@@ -0,0 +1,25 @@
+package atividade08;
+import java.util.Scanner;
+public class Calculo8 {
+	Scanner entrada = new Scanner(System.in);
+	double raio;
+	double volume;
+    
+	
+	public void caucularRaio() {
+		
+        System.out.println("insira o raio da esfera");
+        raio = entrada.nextDouble();
+
+        volume = 4 * 3.14 * raio * raio *raio / 3;
+
+	    if(raio < 0){
+	        System.out.println("Burro!!!");
+	    }
+	    
+	    else{
+	        System.out.println("volume da esfera\n= " + volume);
+	    }
+
+	}
+}
